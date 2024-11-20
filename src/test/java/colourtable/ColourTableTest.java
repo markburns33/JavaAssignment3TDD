a61d1035b437f6501c6264489b59985cb8b50535
Mark Burns <maekburns@gmail.com>
2024-11-20 16:50:30 +0000

    Added comments to test cases

--- src/test/java/colourtable/ColourTableTest.java
+++ src/test/java/colourtable/ColourTableTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ColourTableTest {
     @Test
     void testValidConstructor() {
+        // Test ColourTable can be made with valid size input
         assertDoesNotThrow(() -> new ColourTable(4));
     }
 
     @Test
     void testInvalidConstructor() {
+        // Test size less than 2
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(1));
+        // Test size with negative integer
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(-8));
+        // Test size with 0
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(0));
+        // Test with size that is not a power of 2
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(9));
     }
+
 
 }
