fdfb858758a96dd2535ee15f0c7e858d537ab807
Mark Burns <maekburns@gmail.com>
2024-11-26 17:40:55 +0000

    Added test for adding valid colours to ColourTable. Changed invalid inputs for ColourTable test to use ParameterisedTest to reduce the amount of lines and increase readability.

--- src/main/java/colourtable/ColourTable.java
+++ src/main/java/colourtable/ColourTable.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 public class ColourTable {
 
+    private final List<int[]> colours;
+
     public ColourTable(int size) {
         if (size < 2 || (size & (size-1)) !=0) {
             throw new IllegalArgumentException("Size must be a power of 2");
         }
+        this.colours = new ArrayList<>();
+    }
+
+    public void add(int red, int green, int blue) {
+        colours.add(new int[]{red, green, blue});
     }
 }
--- src/test/java/colourtable/ColourTableTest.java
+++ src/test/java/colourtable/ColourTableTest.java
@@ -1,24 +1,35 @@
 package colourtable;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ColourTableTest {
+
+    // Test ColourTable can be made with valid size input
     @Test
-    void testValidConstructor() {
-        // Test ColourTable can be made with valid size input
+    void testValidConstructor(){
         assertDoesNotThrow(() -> new ColourTable(4));
     }
 
+    // Test invalid inputs for ColourTable
+    @ParameterizedTest
+    @ValueSource(ints={1,-8,0,9})
+    void testInvalidConstructor(int size) {
+        assertThrows(IllegalArgumentException.class,() -> new ColourTable(size));
+    }
+
+    // Test adding valid colours to ColourTable
     @Test
-    void testInvalidConstructor() {
-        // Test size less than 2
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(1));
-        // Test size with negative integer
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(-8));
-        // Test size with 0
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(0));
-        // Test with size that is not a power of 2
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(9));
+    void testAddValidColour() {
+        // New table object with size of 4
+        ColourTable table = new ColourTable(4);
+
+        assertDoesNotThrow(() -> table.add(255,0,0)); // Red
+        assertDoesNotThrow(() -> table.add(0,255,0)); // Green
+        assertDoesNotThrow(() -> table.add(0,0,255)); // Blue
+        assertDoesNotThrow(() -> table.add(255,255,255)); // White
+
     }
 
 
